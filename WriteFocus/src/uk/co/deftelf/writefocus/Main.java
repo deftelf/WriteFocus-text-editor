420fe67ab8c74ec063127be74a476017957b6303
Carl Fletcher <carl@deftelf.co.uk>
2011-06-23 13:22:46 +0100

    Search

--- WriteFocus/src/uk/co/deftelf/writefocus/Main.java
+++ WriteFocus/src/uk/co/deftelf/writefocus/Main.java
@@ -21,12 +21,16 @@ import android.text.Editable;
 import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.Scroller;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,6 +45,7 @@ public class Main extends Activity {
     private Handler helpHintHide;
     private ArrayDeque<Undo> undoHistory = new ArrayDeque<Undo>();
     private boolean undoing = false;
+    private Toast notFound;
     
     
     private class Undo {
@@ -67,6 +72,7 @@ public class Main extends Activity {
         helpHints = (TextView) findViewById(R.id.helphints);
         text.init(this);
         
+        notFound = Toast.makeText(this, "Not found", Toast.LENGTH_SHORT);
         StringBuilder testText = new StringBuilder();
         try {
             InputStream fs = getAssets().open("pg2600.txt");
@@ -78,7 +84,7 @@ public class Main extends Activity {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        //text.setText(testText);
+        text.setText(testText);
         text.setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
         
         try {
@@ -149,6 +155,58 @@ public class Main extends Activity {
                 lc++;
         }
         return lc;
+        
+        
+    }
+
+    public int search(String search) {
+        if (search.length() == 0)
+            return -1;
+        CharSequence contents = text.getText();
+        int cursorAt = 0;//text.getSelectionEnd();
+        boolean found = false;
+        while (!found && cursorAt < contents.length()) {
+            int i = 0;
+            for (; cursorAt < contents.length() && i < search.length(); cursorAt++) {
+                char c = Character.toLowerCase(search.charAt(i));
+                if (cursorAt < contents.length() &&
+                        c == Character.toLowerCase(contents.charAt(cursorAt))) {
+                    i++;
+                    found = true;
+                } else {
+                    found = false;
+                    cursorAt++;
+                    break;
+                }
+            }
+            if (found)
+                return cursorAt - search.length();
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            
+            public boolean onQueryTextSubmit(String arg0) {
+                
+                return false;
+            }
+            
+            public boolean onQueryTextChange(String arg0) {
+                int loc = search(arg0);
+                Log.d("search", loc + "");
+                if (loc == -1)
+                    notFound.show();
+                else 
+                    text.setSelection(loc);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
     
     public void showStats() {
@@ -215,22 +273,6 @@ public class Main extends Activity {
         });
         helpHintHide.sendEmptyMessageDelayed(0, 5000);
     }
-
-//    public int lineCount() {
-//        int lc = 0;
-//        Editable ed = text.getText();
-//        boolean line = false;
-//        for (int i=0; i < ed.length(); i++) {
-//            char ch = ed.charAt(i);
-//            if (line &&
-//                    ch == '\n') {
-//                line = false;
-//                lc++;
-//            } else if (!line && 
-//                    ch != '\n') {
-//                line = true;
-//            }
-//        }
-//        return lc;
-//    }
+    
+    
 }
